fb6b68b49ab5130c387a5ff81a26e36debfe7455
Glenn Gillen <me@glenngillen.com>
2015-05-29 09:54:25 +1000

    Make class for highlighting match css.

--- lib/nesta-contentfocus-extensions/page.rb
+++ lib/nesta-contentfocus-extensions/page.rb
@@ -25,7 +25,7 @@ module Nesta
       end
 
       def block_code(code, language)
-        options = {options: { linenos: true }}
+        options = {options: { linenos: true, cssclass: "hll" }}
         options.merge!(lexer: language) if LANGUAGES.include? language
         highlighted_code = Pygments.highlight(code, options)
         highlighted_code
