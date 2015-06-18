1589172744a1cabb7649ea9c09e07bf0462ee3cc
Glenn Gillen <me@glenngillen.com>
2015-06-18 17:09:20 +1000

    Iterating over the wrong paths.

--- lib/nesta-contentfocus-extensions/overrides.rb
+++ lib/nesta-contentfocus-extensions/overrides.rb
@@ -3,7 +3,7 @@ module Nesta
   module Overrides
     private
     def self.render_options(template, *engines)
-      Nesta::ContentFocus::Paths.public_paths.each do |path|
+      Nesta::ContentFocus::Paths.view_paths.each do |path|
         engines.each do |engine|
           if template_exists?(engine, path, template)
             return { views: path }, engine
