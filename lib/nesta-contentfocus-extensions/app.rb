dca62ade5757146ff553a216222da0dba7243e98
Glenn Gillen <me@glenngillen.com>
2015-06-18 20:50:45 +1000

    Need to inherit from the correct base class.

--- lib/nesta-contentfocus-extensions/app.rb
+++ lib/nesta-contentfocus-extensions/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra/flash'
 require 'nesta-contentfocus-extensions/routes'
 module Nesta
-  class App
+  class App < Sinatra::Base
     register Sinatra::Flash
     include Nesta::ContentFocus::Routes
   end
