a8673fc8a2a4bf7d8972d814db82b83b84acd066
Glenn Gillen <me@glenngillen.com>
2015-07-01 12:03:14 +1000

    More extraction of common theme setup steps.

---
+++ lib/nesta-contentfocus-extensions/theme.rb
@@ -0,0 +1,32 @@
+require 'nesta-contentfocus-extensions/paths'
+module Nesta
+  module ContentFocus
+    module Theme
+      def register(name, paths = {})
+        if paths[:base]
+          base_path = paths[:base]
+          view_path = File.expand_path(base_path + "/views")
+          stylesheet_path = File.expand_path(base_path + "/stylesheets")
+        end
+        stylesheet_path = paths[:styles]
+        view_path = paths[:views]
+        if stylesheet_path
+          Nesta::ContentFocus::Paths.add_sass_path(stylesheet_path)
+          Nesta::ContentFocus::Paths.add_view_path(stylesheet_path)
+        end
+        if view_path
+          Nesta::ContentFocus::Paths.add_view_path(view_path)
+          Nesta::ContentFocus::Paths.add_view_path(File.expand_path(view_path + "/#{name}"))
+        end
+        if path[:public]
+        end
+      end
+    end
+  end
+end
+
+module Nesta
+  module Theme
+    extend Nesta::ContentFocus::Theme
+  end
+end
