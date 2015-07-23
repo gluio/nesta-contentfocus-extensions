38a152dc9e215d8e62ae880fd67bd0d61eceabdc
Glenn Gillen <me@glenngillen.com>
2015-07-23 11:05:13 +1000

    Remove debugging.

--- lib/nesta-contentfocus-extensions/page.rb
+++ lib/nesta-contentfocus-extensions/page.rb
@@ -3,7 +3,6 @@ module Nesta
   class Page < FileModel
     def convert_to_html(format, scope, text)
       text = add_p_tags_to_haml(text) if @format == :haml
-      STDOUT.puts markdown_options
       template = Tilt[format].new(nil, 1, markdown_options) { text }
       template.render(scope)
     end
