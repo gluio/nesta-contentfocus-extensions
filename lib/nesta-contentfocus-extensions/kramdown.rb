e5e060e2e841fdaee7964ac084f5c1b28d5ff4f9
Glenn Gillen <me@glenngillen.com>
2015-07-23 11:54:29 +1000

    Change way classes are loaded to make autoload play nice.

--- lib/nesta-contentfocus-extensions/kramdown.rb
+++ lib/nesta-contentfocus-extensions/kramdown.rb
@@ -1,5 +1,6 @@
 require 'rouge'
 require 'kramdown'
+require 'kramdown/converter'
 require 'tilt'
 
 Tilt.register Tilt::KramdownTemplate, 'markdown', 'mkd', 'md'
@@ -11,6 +12,8 @@ module Kramdown
       def self.call(converter, text, lang, type, _unused_opts)
         opts = converter.options[:syntax_highlighter_opts].dup
         lexer = ::Rouge::Lexer.find_fancy(lang || opts[:default_lang], text)
+        STDOUT.puts "Code\n\n"
+        STDOUT.puts type
         return nil unless lexer
         if type == :span
           opts[:wrap] = false
@@ -23,20 +26,25 @@ module Kramdown
   end
 
   module Converter
+    add_syntax_highlighter :rouge do |converter, text, lang, type, opts|
+      add_syntax_highlighter :rouge, ::Kramdown::SyntaxHighlighter::Rouge
+      syntax_highlighter(:rouge).call(converter, text, lang, type, opts)
+    end
+
     class Html < Base
       alias_method :pre_headstartup_convert_ul, :convert_ul
       alias_method :pre_headstartup_convert_blockquote, :convert_blockquote
       alias_method :pre_headstartup_convert_header, :convert_header
 
       def convert_ul(el, indent)
-        if ["benefits", "how", "features"].include? el.attr["class"]
+        if ['benefits', 'how', 'features'].include? el.attr['class']
           el.children.each do |li|
             p = li.children.detect{ |c| c.type == :p }
             if p
               img = p.children.detect{ |c| c.type == :img}
               if img
-                description = Element.new(:html_element, "div", :class => "description")
-                example = Element.new(:html_element, "div", :class => "example")
+                description = Element.new(:html_element, 'div', :class => 'description')
+                example = Element.new(:html_element, 'div', :class => 'example')
                 p.children.delete(img)
                 example.children = [img]
                 description.children = li.children
@@ -50,7 +58,7 @@ module Kramdown
       end
 
       def convert_blockquote(el, indent)
-        if el.attr["class"] == "testimonial"
+        if el.attr['class'] == 'testimonial'
           p = el.children.detect{ |c| c.type == :p }
           if p
             mdash_idx = p.children.index{ |c| c.type == :typographic_sym && c.value == :mdash }
@@ -58,7 +66,7 @@ module Kramdown
               next_el = p.children[mdash_idx + 1]
               if (next_el.type == :text) && (next_el.value.strip.empty?) && (p.children[mdash_idx + 2].type == :a)
                 p.children.delete_at(mdash_idx)
-                cite = Element.new(:html_element, "cite")
+                cite = Element.new(:html_element, 'cite')
                 cite.children = p.children.pop(p.children.size - mdash_idx)
                 p.children.push cite
               elsif next_el.type == :a
@@ -72,10 +80,10 @@ module Kramdown
       def convert_header(el, indent)
         output = pre_headstartup_convert_header(el, indent)
         matched, open, content, close = *output.match(%r{(.*<h[1-6][^>]*>)(.*)(</h[1-6]>)})
-        split_content = content.split(" ")
+        split_content = content.split(' ')
         if split_content.size > 3
-          split_content[-1] = [split_content[-2], "&nbsp;", split_content.pop].join
-          content = split_content.join(" ")
+          split_content[-1] = [split_content[-2], '&nbsp;', split_content.pop].join
+          content = split_content.join(' ')
         end
         [open, content, close].join
       end
