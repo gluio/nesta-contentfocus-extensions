bb463669739f3e253d93f3c5002271b08fa39b7b
Glenn Gillen <me@glenngillen.com>
2015-07-28 09:53:37 +1000

    Add line number class directly in Rouge formatter.

--- lib/nesta-contentfocus-extensions/html_syntax_formatter.rb
+++ lib/nesta-contentfocus-extensions/html_syntax_formatter.rb
@@ -3,6 +3,26 @@ require 'nesta-contentfocus-extensions/rouge/formatters/html'
 module Nesta
   module ContentFocus
     class HTMLSyntaxFormatter < ::Rouge::Formatters::HTML
+      attr_reader :options
+
+      def initialize(opts = {})
+        super(opts)
+        @options = opts
+      end
+
+      def stream(tokens, &blk)
+        yield '<span class="line">'
+        tokens.each do |tok, val|
+          val.scan /\n|[^\n]+/ do |s|
+            if s == "\n"
+              yield %Q{</span>\n<span class="line">}
+            else
+              yield span(tok, s)
+            end
+          end
+        end
+        yield "</span>"
+      end
     end
   end
 end
