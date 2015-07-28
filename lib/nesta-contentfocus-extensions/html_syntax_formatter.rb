d4f675e8102dbc4990c9ca022bc68915f60da80e
Glenn Gillen <me@glenngillen.com>
2015-07-28 11:17:38 +1000

    Don't need rouge plugin anymore.

--- lib/nesta-contentfocus-extensions/html_syntax_formatter.rb
+++ lib/nesta-contentfocus-extensions/html_syntax_formatter.rb
@@ -1,5 +1,4 @@
 require 'nesta-contentfocus-extensions/kramdown/kramdown'
-require 'nesta-contentfocus-extensions/rouge/formatters/html'
 module Nesta
   module ContentFocus
     class HTMLSyntaxFormatter < ::Rouge::Formatters::HTML
