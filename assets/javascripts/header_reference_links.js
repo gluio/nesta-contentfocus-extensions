dcef5e7cba78afa0dc1c02f656309a25532c1632
Glenn Gillen <me@glenngillen.com>
2015-08-04 22:24:44 +1000

    Can only run script when DOM is ready.

--- assets/javascripts/header_reference_links.js
+++ assets/javascripts/header_reference_links.js
@@ -1,11 +1,13 @@
-jQuery('h1[id], h2[id], h3[id], section[id] > h1, section[id] > h2, section[id] > h3').each(function() {
-  var ref;
-  var el = jQuery(this);
-  if (el.attr('id')) {
-    ref = el.attr('id');
-  } else {
-    ref = el.parent().attr('id');
-  }
-  var link = jQuery('<a href="#'+ref+'" class="icon-link"></a>');
-  el.prepend(link);
+jQuery(document).ready(function() {
+  jQuery('h1[id], h2[id], h3[id], section[id] > h1, section[id] > h2, section[id] > h3').each(function() {
+    var ref;
+    var el = jQuery(this);
+    if (el.attr('id')) {
+      ref = el.attr('id');
+    } else {
+      ref = el.parent().attr('id');
+    }
+    var link = jQuery('<a href="#'+ref+'" class="icon-link"></a>');
+    el.prepend(link);
+  });
 });
