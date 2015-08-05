f83087c0c607b07e9cfd37e3af5535b644c39eb1
Glenn Gillen <me@glenngillen.com>
2015-08-05 16:04:37 +1000

    Don't add reference links to feedback callout.

--- assets/javascripts/header_reference_links.js
+++ assets/javascripts/header_reference_links.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function() {
   jQuery('h1[id], h2[id], h3[id], section[id] > h1, section[id] > h2, section[id] > h3').each(function() {
     var ref;
     var el = jQuery(this);
+    if (el.parent().is('#feedback')) { return true; }
     if (el.attr('id')) {
       ref = el.attr('id');
     } else {
