ef407cc39b05bdc89b2f84cc4b27a92ebd043a52
Glenn Gillen <me@glenngillen.com>
2015-08-05 19:40:19 +1000

    Missing value for test.

--- assets/javascripts/scroll.js
+++ assets/javascripts/scroll.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function() {
   if (location.hash && nav.length > 0) {
     setTimeout(function() {
       var el = jQuery(location.hash);
-      if (el.length > ) {
+      if (el.length > 0) {
         var scrollTo = el.position().top - nav.height() - 33;
         jQuery('html, body').animate({ scrollTop: scrollTo }, 1000);
       }
