bde869b427a6b47989e325e6719215b3dda24689
Glenn Gillen <me@glenngillen.com>
2015-08-05 19:35:23 +1000

    Add script to accomodate nav when scrolling.

---
+++ assets/javascripts/scroll.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+  var nav = jQuery('header.navigation');
+  if (location.hash && nav.length > 0) {
+    setTimeout(function() {
+      var el = jQuery(location.hash);
+      if (el.length > ) {
+        var scrollTo = el.position().top - nav.height() - 33;
+        jQuery('html, body').animate({ scrollTop: scrollTo }, 1000);
+      }
+    }, 1000);
+  }
+});
