496a63bdf1bbf5e41b38a3cf9ba26041833a3f38
Glenn Gillen <me@glenngillen.com>
2016-02-22 13:17:05 +1100

    I don't think we need this with the greedy route gone.

--- lib/nesta-contentfocus-extensions/routes.rb
+++ lib/nesta-contentfocus-extensions/routes.rb
@@ -9,32 +9,6 @@ module Nesta
             stylesheet(sheet.to_sym)
           end
 
-          get '/robots.txt' do
-            content_type 'text/plain', charset: 'utf-8'
-            <<-EOF
-# robots.txt
-# See http://en.wikipedia.org/wiki/Robots_exclusion_standard
-            EOF
-          end
-
-          get '/articles.xml' do
-            content_type :xml, charset: 'utf-8'
-            set_from_config(:title, :subtitle, :author)
-            @articles = Page.find_articles.select { |a| a.date }[0..9]
-            haml(:atom, format: :xhtml, layout: false)
-          end
-
-          get '/sitemap.xml' do
-            content_type :xml, charset: 'utf-8'
-            @pages = Page.find_all.reject do |page|
-              page.draft? or page.flagged_as?('skip-sitemap')
-            end
-            @last = @pages.map { |page| page.last_modified }.inject do |latest, page|
-              (page > latest) ? page : latest
-            end
-            haml(:sitemap, format: :xhtml, layout: false)
-          end
-
           get '*' do
             pass unless params.key?('draft')
             set_common_variables
